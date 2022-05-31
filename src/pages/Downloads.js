f725f6012481e584e9caef51a84012f048fd24a5
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-05-31 22:58:50 +0100

    complete the download page

--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -1,7 +1,178 @@
 const Downloads = () => {
     return (
         <>
-            
+            <div className="container"></div>
+            <div className="layout clearfix">
+                <section className="layout-content">
+                    <h3 id="v8.1.6" className="title">
+                        <span className="release-state">Current Stable</span>
+                        PHP 8.1.6    (<a href="/ChangeLog-8.php#8.1.6" className="changelog">Changelog</a>)
+                    </h3>
+                    <div className="content-box">
+                        <ul>
+                            <li>
+                                <a href="/distributions/php-8.1.6.tar.gz">php-8.1.6.tar.gz</a>
+                                <a href="/distributions/php-8.1.6.tar.gz.asc">(sig)</a> 
+                                [19,233Kb] <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">e847745fd66fc8c57fac993a609fefcded93fddccd225f0620a26bb5ae5753c3</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-8.1.6.tar.bz2">php-8.1.6.tar.bz2</a>
+                                <a href="/distributions/php-8.1.6.tar.bz2.asc">(sig)</a> 
+                                [15,016Kb] <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">7b353304b7407554f70d3e101a226a1fc22decae5c4c42ed270c4e389bfa1b66</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-8.1.6.tar.xz">php-8.1.6.tar.xz</a> <a href="/distributions/php-8.1.6.tar.xz.asc">(sig)</a> 
+                                [11,434Kb] <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">da38d65bb0d5dd56f711cd478204f2b62a74a2c2b0d2d523a78d6eb865b2364c</span>
+                            </li>
+                            <li>
+                                <a href="https://windows.php.net/download#php-8.1">
+                                    Windows downloads
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="#gpg-8.1">GPG Keys for PHP 8.1</a>
+                    </div>
+                    <h3 id="v8.0.19" className="title">
+                        <span className="release-state">Old Stable</span>
+                        PHP 8.0.19    (<a href="/ChangeLog-8.php#8.0.19" className="changelog">Changelog</a>)
+                    </h3>
+                    <div className="content-box">
+                        <ul>
+                            <li>
+                                <a href="/distributions/php-8.0.19.tar.gz">php-8.0.19.tar.gz</a> <a href="/distributions/php-8.0.19.tar.gz.asc">(sig)</a> [16,055Kb]          <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">48e57634d350bcab4745d25d9d94ffa474649bf4f7e879fad163226c0d107bb5</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-8.0.19.tar.bz2">php-8.0.19.tar.bz2</a> <a href="/distributions/php-8.0.19.tar.bz2.asc">(sig)</a> [12,856Kb]          <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">eba0e67fdaf6904b2e4b84e064be0a0d61b2cb64a23f81a0ca9b1a51bc3a8330</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-8.0.19.tar.xz">php-8.0.19.tar.xz</a> <a href="/distributions/php-8.0.19.tar.xz.asc">(sig)</a> [10,526Kb]          <span className="releasedate">12 May 2022</span>
+                                <span className="sha256">ba62219c4b0486cbb2a04f0796749a46b0ee1f5a142ed454212b4e2460cb0fab</span>
+                            </li>
+                            <li>
+                                <a href="https://windows.php.net/download#php-8.0">
+                                    Windows downloads
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="#gpg-8.0">GPG Keys for PHP 8.0</a>
+                    </div>
+                    <a id="v7"></a>
+                    <h3 id="v7.4.29" className="title">
+                        <span className="release-state">Old Stable</span>
+                        PHP 7.4.29    (<a href="/ChangeLog-7.php#7.4.29" className="changelog">Changelog</a>)
+                    </h3>
+                    <div className="content-box">
+                        <ul>
+                            <li>
+                                <a href="/distributions/php-7.4.29.tar.gz">php-7.4.29.tar.gz</a> <a href="/distributions/php-7.4.29.tar.gz.asc">(sig)</a> [16,471Kb]          <span className="releasedate">14 Apr 2022</span>
+                                <span className="sha256">f73f89873bb9447cb99eb4863cf0a0deab4481cb8acf7552c0e70647e6885854</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-7.4.29.tar.bz2">php-7.4.29.tar.bz2</a> <a href="/distributions/php-7.4.29.tar.bz2.asc">(sig)</a> [12,717Kb] <span className="releasedate">14 Apr 2022</span>
+                                <span className="sha256">7dde58a02b225c25130c6e2ae2cbba7254bb0340f7fe17291478176d866f9482</span>
+                            </li>
+                            <li>
+                                <a href="/distributions/php-7.4.29.tar.xz">php-7.4.29.tar.xz</a> <a href="/distributions/php-7.4.29.tar.xz.asc">(sig)</a> [10,175Kb]          <span className="releasedate">14 Apr 2022</span>
+                                <span className="sha256">7d0f07869f33311ff3fe1138dc0d6c0d673c37fcb737eaed2c6c10a949f1aed6</span>
+                            </li>
+                            <li>
+                                <a href="https://windows.php.net/download#php-7.4">
+                                    Windows downloads
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="#gpg-7.4">GPG Keys for PHP 7.4</a>
+                    </div>
+                    <hr />
+                    <h2>GPG Keys</h2>
+                    <p>
+                        The releases are tagged and signed in the <a href="git.php">PHP Git Repository</a>.
+                        The following official GnuPG keys of the current PHP Release Manager can be used
+                        to verify the tags:
+                    </p>
+                    <h3 id="gpg-8.1" className="content-header">
+                        PHP 8.1
+                    </h3>
+                    <div className="content-box">
+                        <pre>
+                        pub   2048R/31CBD89E 2016-12-08 <br />
+                                Key fingerprint = 5289 95BF EDFB A719 1D46  839E F9BA 0ADA 31CB D89E<br />
+                        uid                  Joe Watkins &lt;krakjoe@php.net&gt;<br />
+                        <br />
+                        pub   rsa4096 2021-04-26 [SC] [expires: 2025-11-24]<br />
+                                39B6 4134 3D8C 104B 2B14  6DC3 F9C3 9DC0 B969 8544<br />
+                        uid           [ultimate] Ben Ramsey &lt;ramsey@php.net&gt;<br />
+                        sub   rsa4096 2021-04-26 [E] [expires: 2025-11-24]<br />
+                        <br />
+                        pub   rsa4096 2021-04-01 [SC]<br />
+                                F1F6 9223 8FBC 1666 E5A5  CCD4 199F 9DFE F6FF BAFD<br />
+                        uid           [ultimate] Patrick Allaert &lt;patrickallaert@php.net&gt;<br />
+                        sub   rsa4096 2021-04-01 [E]<br />
+                        <br />
+                        </pre>
+                    </div>
+                    <h3 id="gpg-8.0" className="content-header">
+                        PHP 8.0
+                    </h3>
+                    <div className="content-box">
+                        <pre>pub   4096R/70D12172 2017-04-14 [expires: 2024-04-21]<br />
+                            Key fingerprint = 1729 F839 38DA 44E2 7BA0  F4D3 DBDB 3974 70D1 2172<br />
+                        uid                  Sara Golemon &lt;pollita@php.net&gt;<br />
+                        <br />
+                        pub   rsa4096 2020-05-09 [SC] [expires: 2024-05-08]<br />
+                            BFDD D286 4282 4F81 18EF  7790 9B67 A5C1 2229 118F<br />
+                        uid           [ultimate] Gabriel Caruso (Release Manager) &lt;carusogabriel@php.net&gt;<br />
+                        sub   rsa4096 2020-05-09 [E] [expires: 2024-05-08]<br /><br />
+                        </pre>
+                    </div>
+                    <h3 id="gpg-7.4" className="content-header">
+                        PHP 7.4
+                    </h3>
+                    <div className="content-box downloads">
+                        <pre>sec   rsa4096 2019-06-11 [SC] [expires: 2029-06-08]<br />
+                            5A52880781F755608BF815FC910DEB46F53EA312<br />
+                        uid           [ultimate] Derick Rethans &lt;gpg@derickrethans.nl&gt;<br />
+                        uid           [ultimate] Derick Rethans &lt;derick@derickrethans.nl&gt;<br />
+                        uid           [ultimate] Derick Rethans (GitHub) &lt;github@derickrethans.nl&gt;<br />
+                        uid           [ultimate] Derick Rethans (PHP) &lt;derick@php.net&gt;<br />
+                        ssb   rsa4096 2019-06-11 [E] [expires: 2029-06-08]<br />
+                        <br />
+                        pub   rsa4096 2019-05-29 [SC] [expires: 2021-05-28]<br />
+                            4267 0A7F E4D0 441C 8E46  3234 9E4F DC07 4A4E F02D<br />
+                        uid           [ultimate] Peter Kokot &lt;petk@php.net&gt;<br />
+                        sub   rsa4096 2019-05-29 [E] [expires: 2021-05-28]<br /><br />
+                        </pre>
+                    </div>
+                    <p>
+                        <a href="gpg-keys.php">
+                            A full list of GPG keys used for current and older releases is also
+                            available.
+                        </a>
+                    </p>
+                </section>
+                <aside className="tips">
+                    <div className="inner">
+                        <div className="panel">
+                            <a href="/supported-versions.php">Supported Versions</a>
+                            <div className="body">
+                                Check the <a href="/supported-versions.php">supported versions</a> page for
+                                more information on the support lifetime of each version of PHP.
+                            </div>
+                        </div>
+
+                        <p className="panel"><a href="download-docs.php">Documentation download</a></p>
+                        <p className="panel"><a href="download-logos.php">PHP logos</a></p>
+
+                        <p className="panel"><a href="/git.php">Development sources (git)</a></p>
+                        <p className="panel"><a href="/releases/">Old archives</a></p>
+                    </div>
+                </aside>
+            </div>
         </>
     )
 }
